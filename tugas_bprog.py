747a0ee67537d74c5cd06e18ce6de6eeeacfeb19
kang azizul <zulslol413@gmal.com>
2021-10-19 11:32:17 +0700

    program python

---
+++ tugas_bprog.py
@@ -0,0 +1,17 @@
+#program input
+print(15*"=")
+
+nama = str(input("nama = "))
+pangilan = str(input("panggilan = "))
+nim = int(input("nim = "))
+tempat_lahir = str(input("tempat lahir = "))
+tanggal_lahir = str(input("tanggal lahir = "))
+umur = int(input("umur = "))
+wa = int(input("nomor telepon = "))
+alamat = str(input("alamat = "))
+
+print(15*"=")
+print("lets me introduce my self. My name is ",nama," but you can call me ",pangilan,".")
+print("My NIM ",nim,". i was born in ",tempat_lahir," and i am ",umur," years old.")
+print("i am very glad if you want to invite my house in ",alamat,". so, dont forget to call me before with the number ",wa,".")
+
